80d177a4cfac72c136ca4dd57b58abb836a427ec
stefanprodan <stefan.prodan@gmail.com>
2020-04-01 11:51:10 +0300

    Remove kustomize

--- scripts/gen-hrs.sh
+++ scripts/gen-hrs.sh
@@ -6,16 +6,12 @@ set -o errexit
 
 COUNT=$1
 NAMESPACE=test
-REPO=https://github.com/stefanprodan/gitops-benchmark
-REPO_ROOT=$(git rev-parse --show-toplevel)
-TEST_DIR="${REPO_ROOT}/cluster/${NAMESPACE}"
-
-mkdir -p ${TEST_DIR}
 
 for i in $(seq -f "%05g" 1 $COUNT)
 do
 
-cat << EOF | tee ${TEST_DIR}/app-${i}.yaml > /dev/null
+cat << EOF
+---
 apiVersion: helm.fluxcd.io/v1
 kind: HelmRelease
 metadata:
@@ -31,13 +27,10 @@ EOF
 
 done
 
-cat << EOF | tee ${TEST_DIR}/namespace.yaml > /dev/null
+cat << EOF
+---
 apiVersion: v1
 kind: Namespace
 metadata:
-  name: test
+  name: ${NAMESPACE}
 EOF
-
-cd ${TEST_DIR} && rm -f kustomization.yaml && kustomize create --autodetect
-
-cd ${REPO_ROOT} && kustomize build .
