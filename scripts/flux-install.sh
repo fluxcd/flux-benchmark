2a6ecf6d9b14ee3056342c599dc399512149f61d
Stefan Prodan <stefan.prodan@gmail.com>
2023-11-04 11:48:34 +0200

    Dedicated node for Flux controllers
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- scripts/flux-install.sh
+++ scripts/flux-install.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 repo_root=$(git rev-parse --show-toplevel)
 
+kubectl label node flux-worker role=flux --overwrite
+kubectl taint nodes flux-worker role=flux:NoSchedule --overwrite
+
 flux install --manifests $repo_root/install-manifests/install
 
 kubectl apply -f $repo_root/install-manifests/cluster-addons
