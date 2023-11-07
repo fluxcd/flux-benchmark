c3c623fbc75d3fa5dc98246bc26e6a2eedcc0849
Stefan Prodan <stefan.prodan@gmail.com>
2023-11-07 22:50:35 +0200

    Consolidate manifests
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- scripts/flux-install.sh
+++ scripts/flux-install.sh
@@ -7,4 +7,4 @@ repo_root=$(git rev-parse --show-toplevel)
 kubectl label node flux-worker role=flux --overwrite
 kubectl taint nodes flux-worker role=flux:NoSchedule --overwrite
 
-flux install --manifests $repo_root/install-manifests/install
+flux install --manifests $repo_root/manifests/install
