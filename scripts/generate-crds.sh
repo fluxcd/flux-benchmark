2d310e52b65936f5de202ef849aa451afd93667f
Shaked Klein Orbach <klein.shaked@gmail.com>
2024-03-12 21:55:54 +0200

    logging typo

--- scripts/generate-crds.sh
+++ scripts/generate-crds.sh
@@ -8,4 +8,4 @@ do
   sed "s/CRD_COUNT/$crd_name/" $template_file > "$tmpdir/generated_crd_$i.yaml"
   kubectl apply -f "$tmpdir/generated_crd_$i.yaml" > /dev/null
 done
-echo "Generated $crds CRDs"
+echo "Generated $CRD_COUNT CRDs"
