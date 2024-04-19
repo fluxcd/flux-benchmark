3a1a0d59e8fac177d5eabb17f2a24cf7e79f6d73
Stefan Prodan <stefan.prodan@gmail.com>
2024-04-20 00:33:22 +0300

    Generate test CRDs
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- scripts/generate-crds.sh
+++ scripts/generate-crds.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 repo_root=$(git rev-parse --show-toplevel)
 template_file="$repo_root/scripts/crd_template.yaml"
-tmpdir="${TMPDIR:-/tmp}"
+tmpdir="${repo_root}/bin/crds"
+mkdir -p "${tmpdir}"
 for ((i=1; i<=$CRD_COUNT; i++))
 do
   crd_name="$i"
